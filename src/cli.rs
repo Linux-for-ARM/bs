e139b9b1966f15a3d63eaf78f559a11a03af89d4
mskrasnov <linuxoid85@gmail.com>
2024-03-16 19:07:30 +0300

    Начало работы над CLI

---
+++ src/cli.rs
@@ -0,0 +1,21 @@
+use clap::Parser;
+use clap::Subcommand;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+pub struct Cli {
+	#[command(subcommand)]
+	pub work_mode: WorkMode,
+}
+
+#[derive(Subcommand)]
+pub enum WorkMode {
+	/// Set up the build system
+	Config,
+
+	/// Prepare the host system for LFA builds
+	ConfigureHost,
+
+	/// Perform the LFA build
+	Build,
+}
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,27 @@ mod conf;
 mod run;
 mod traits;
 
+mod cli;
 mod ui;
 
 use std::env::var;
+use clap::Parser;
+
+use cli::WorkMode;
 
 fn main() {
-    ui::menuconfig_window();
-    println!(
-        "{}/{}",
-        var("LFA_BS_MBOARD").unwrap(),
-        var("LFA_BS_KERNEL").unwrap()
-    );
+    // ui::menuconfig_window();
+    let cli = cli::Cli::parse();
+    match cli.work_mode {
+        WorkMode::Config => {
+            ui::menuconfig_window();
+            println!(
+                "{}/{}",
+                var("LFA_BS_MBOARD").unwrap(),
+                var("LFA_BS_KERNEL").unwrap()
+            );
+        },
+        WorkMode::ConfigureHost => todo!(),
+        WorkMode::Build => todo!(),
+    }
 }
